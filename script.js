39a06cf230e09332d241ac7216c98979dda0088c
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-12 14:33:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+let charCount = {}
+	for(int i=0;i<str.length ; i++){
+			if (charCount.hasOwnproperty(str[i])){
+					charCount[str[i]]++
+			}else{
+					charCount[str[i]] = 1
+}
+	}
+	for( let key in charCount){
+		if(charCount[key] === 1 ){
+				return key
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
